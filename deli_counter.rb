cf2b38589952c9a76c1842a1497bbd1b1c4fee1e
Emily Morgan <emorgan05@gmail.com>
2017-09-18 18:44:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is empty"
+  else
+    counter = 1
+
+    while array.count < counter do
+      puts "The line is currently: #{counter}: #{katz_deli[counter]}"
+      counter += 1
+    end
+    end
+  end
+end

e70af110a18abc22788b0bde60203ed9f568388b
Emily Morgan <emorgan05@gmail.com>
2017-09-18 18:58:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,16 @@
-def line(katz_deli)
-  if katz_deli.count == 0
+def line(deli)
+  if deli.count == 0
     puts "The line is currently empty."
   else
     counter = 1
 
-    while katz_deli.count < counter do
-      puts "The line is currently: #{counter}. #{katz_deli[counter - 1]}"
+    while deli.count < counter do
+      puts "The line is currently: #{counter}. #{deli[counter - 1]}"
       counter += 1
     end
   end
+end
+
+def take_a_number(deli, name)
+  deli << name
 end
